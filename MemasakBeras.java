f2a6c128f80fa71f829af37509c0b899b23e7ea8
Qudrotulichsan <32732522+Qudrotulichsan@users.noreply.github.com>
2017-10-18 16:39:27 +0700

    Add files via upload

---
+++ MemasakBeras.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package memasak.beras;
+
+/**
+ *
+ * @author HORUS
+ */
+public class MemasakBeras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Cara Memasak Beras"); 
+	System.out.println("Berikut adalah Cara Memasak Beras Menggunakan Magicom :");
+	System.out.println("1. Ukur beras menggunakan cangkir tangkai dan Masukan kedalam Templon ( Penanak nasi) ");
+	System.out.println("2. Cuci Beras Jika Perlu "); 
+	System.out.println("3. Masukan air sampai 2 atau 3 cm di atas beras");
+	System.out.println("4. Masukan templon atau penanak nasi kedalam magicom dan jangan lupa untuk mengaktifkan magicomnya");
+	System.out.println("5. Tunggu beras 10-15 Menit maka beras pun akan berubah menjadi Nasi yang pulen ");
+	System.out.println("SELAMAT MENCOBA !!!! :* "); }
+    
+}
